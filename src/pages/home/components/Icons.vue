2c05f217d2432ccc9e20c15d7ad87189bacb0221
donglongfei <donglongfei2008@sina.com>
2018-12-21 01:04:03 +0800

    add iconList&swiper

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="icon-wrap">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page, index) in pages" :key="index">
+        <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" />
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcon',
+  data () {
+    return {
+      iconList: [
+        {
+          id: 1001,
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: 1002,
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '北京必游'
+        },
+        {
+          id: 1003,
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+          desc: '一日游'
+        },
+        {
+          id: 1004,
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/f6/e54fad3ea337b02.gif',
+          desc: '年终大促'
+        },
+        {
+          id: 1005,
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+          desc: '动植物园'
+        },
+        {
+          id: 1006,
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+          desc: '故宫'
+        },
+        {
+          id: 1007,
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/fc/b10a6b2e4f0fe102.png',
+          desc: '崇礼滑雪'
+        },
+        {
+          id: 1008,
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/fc/b10a6b2e4f0fe102.png',
+          desc: '北京滑雪'
+        },
+        {
+          id: 1009,
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png',
+          desc: '泡温泉'
+        },
+        {
+          id: 1010,
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/80/416c6ab3368d1f02.png',
+          desc: '全部玩乐'
+        }
+      ],
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      }
+    }
+  },
+  computed: {
+    pages () {
+      // icon列表每页显示8条，这里借助二维数组的概念分别存放
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
+  .icon-wrap >>> .swiper-container
+    height 0
+    padding-bottom 56%
+  .icon-wrap
+    margin-top .10rem
+    // background gold
+    .icon
+      overflow hidden
+      height 0
+      float left
+      width 25%
+      padding-bottom 25%
+      // background red
+      position relative
+      text-align center
+      .icon-img
+        position absolute
+        left 0
+        top 0
+        right 0
+        bottom .44rem
+        // background blue
+        box-sizing border-box
+        padding .1rem
+        .icon-img-content
+          height 100%
+      .icon-desc
+        position absolute
+        left 0
+        right 0
+        bottom 0
+        height .44rem
+        line-height .44rem
+        color $darkTextColor
+        ellipsis()
+</style>
