e288dac43e02b2ac2030500e8025e41b7a29a437
donglongfei <donglongfei2008@sina.com>
2018-12-25 09:40:15 +0800

    search finish

--- src/pages/city/components/search.vue
+++ src/pages/city/components/search.vue
@@ -1,12 +1,61 @@
 <template>
-  <div class="search">
-    <input type="text" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input type="text" placeholder="输入城市名或拼音" v-model="keyword">
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有匹配的数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -24,4 +73,18 @@ export default {
       padding 0 .1rem
       border-radius .1rem
       text-align center
+  .search-content
+    overflow hidden
+    position absolute
+    left 0
+    top 1.68rem
+    right 0
+    bottom 0
+    z-index 1
+    background #eee
+    .search-item
+      line-height .60rem
+      padding-left .2rem
+      background #fff
+      color $darkTextColor
 </style>
